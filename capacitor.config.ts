f9b6a7cf398ed584d67130f8e9e30976f7f8915d
nnivxix <adhe.hanasa@gmail.com>
2023-05-20 11:40:21 +0700

    feat: add config capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.tagihlunas.app",
+  appName: "tagihlunas",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
