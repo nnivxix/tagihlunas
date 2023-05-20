6ef58af22b3e80256904fb2c0b157fd24ea57881
nnivxix <adhe.hanasa@gmail.com>
2023-05-20 12:39:18 +0700

    fix: time format on card transaction

--- src/components/CardTransaction.vue
+++ src/components/CardTransaction.vue
@@ -1,19 +1,33 @@
 <template>
-  <router-link :to="{name: 'transactions.show', params: {userId: userId, trxId: trxId}}"
-  class="grid grid-rows-2 grid-cols-2 cursor-pointer bg-semi-gray-1 px-8 py-4 text-dark hover:text-gray-700 hover:bg-lemon">
-    <p class="text-sm font-semibold">{{ trxId }} </p>
-    <p id="amount" class="text-lg font-semibold text-gray-800 text-end row-span-2 self-center">{{ amount.toLocaleString('id-ID', {style: 'currency', currency: 'IDR'}) }}</p>
-    <p class="text-sm text-gray-400">{{`${useMonth((new Date(dateTrx as string).getMonth()+1))}, ${ new Date(dateTrx as string).getDate()}-${new Date(dateTrx as string).getFullYear()} ${new Date(dateTrx as string).getHours()}:${new Date(dateTrx as string).getMinutes()}`}}</p>
+  <router-link
+    :to="{
+      name: 'transactions.show',
+      params: { userId: userId, trxId: trxId },
+    }"
+    class="grid grid-rows-2 grid-cols-2 cursor-pointer bg-semi-gray-1 px-8 py-4 text-dark hover:text-gray-700 hover:bg-lemon"
+  >
+    <p class="text-sm font-semibold">{{ trxId }}</p>
+    <p
+      id="amount"
+      class="text-lg font-semibold text-gray-800 text-end row-span-2 self-center"
+    >
+      {{
+        amount.toLocaleString("id-ID", { style: "currency", currency: "IDR" })
+      }}
+    </p>
+    <p class="text-sm text-gray-400">
+      {{ timeFormated(dateTrx as string, "YYYY-MM-DD HH:mm:ss") }}
+    </p>
   </router-link>
 </template>
 
 <script setup lang="ts">
-import useMonth from '@/composables/useMonth';
+import { timeFormated } from "@/composables/useTime";
 
 defineProps({
   trxId: {
     type: String,
-    default: '',
+    default: "",
   },
   amount: {
     type: Number,
@@ -26,9 +40,4 @@ defineProps({
     type: String,
   },
 });
-
 </script>
-
-<style scoped>
-
-</style>
