7bff4935d67af990e1cbbb4cc67adc853c10361b
nnivxix <adhe.hanasa@gmail.com>
2023-06-18 00:27:09 +0700

    fix: initial name

--- src/components/TheAvatar.vue
+++ src/components/TheAvatar.vue
@@ -4,18 +4,21 @@
     :style="`background-color:${background}; border-radius:100%;`"
     class="text-gray-100 p-5 min-w-[75px] aspect-square flex flex-col justify-center items-center"
   >
-    <p id="initial" class="font-semibold text-2xl">{{ getInitial(name) }}</p>
+    <p id="initial" class="font-semibold text-2xl">{{ initialName }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import useInitialName from "@/composables/useInitialName";
 
-const { getInitial } = useInitialName();
 // eslint-disable-next-line vue/no-setup-props-destructure
 const { name, dimension, background } = defineProps<{
   name: string;
   dimension?: number;
   background?: string;
 }>();
+const { getInitial } = useInitialName();
+
+const initialName = ref(getInitial(name as string));
 </script>
