a7ff0ca81aa3e75e4ce5bdc2bff5b290f64b2352
nnivxix <adhe.hanasa@gmail.com>
2024-02-04 14:59:42 +0700

    chore: remove font-awesome

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,4 @@
 import { createApp } from "vue";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-  faEye,
-  faEyeSlash,
-  faPlus,
-  faArrowRightFromBracket,
-  faArrowLeft,
-  faPen,
-  faXmark,
-  faArrowDownAZ,
-  faEllipsisVertical,
-  faX,
-  faArrowUpFromBracket,
-  faCopy,
-} from "@fortawesome/free-solid-svg-icons";
 import { createPinia } from "pinia";
 import "./style.css";
 import App from "@/App.vue";
@@ -23,27 +7,11 @@ import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import GuestLayout from "@/layouts/GuestLayout.vue";
 import Icon from "@/components/Icon.vue";
 
-library.add(
-  faEye,
-  faEyeSlash,
-  faPlus,
-  faArrowRightFromBracket,
-  faArrowLeft,
-  faPen,
-  faXmark,
-  faArrowDownAZ,
-  faEllipsisVertical,
-  faX,
-  faArrowUpFromBracket,
-  faCopy
-);
-
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
-app.component("font-awesome-icon", FontAwesomeIcon);
 app.component("DefaultLayout", DefaultLayout);
 app.component("GuestLayout", GuestLayout);
 app.component("Icon", Icon);
